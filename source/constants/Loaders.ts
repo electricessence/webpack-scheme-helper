453d4a157b22c5bb46194ea2bec6cba9ea5bfff9
electricessence <oren@electrified.net>
2017-12-11 20:09:12 -0800

    Update Loaders.ts

--- source/constants/Loaders.ts
+++ source/constants/Loaders.ts
@@ -1,9 +1,10 @@
 export const TS:string = "ts-loader";
 export const FILE:string = "file-loader";
+export const URL:string = "url-loader";
 export const STYLE:string = "style-loader";
 export const SCSS:string = "scss-loader";
 export const LESS:string = "less-loader";
 export const CSS:string = "css-loader";
-export const CSS:string = "html-loader";
+export const HTML:string = "html-loader";
 
 Object.freeze(exports);
